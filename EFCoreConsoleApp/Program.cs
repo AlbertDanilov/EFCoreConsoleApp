ef0dfe590f94df921f60d9d985620b34626bedab
Danilov <albert95danilov@gmail.com>
2023-08-28 18:06:31 +0300

    show users in db

--- EFCoreConsoleApp/Program.cs
+++ EFCoreConsoleApp/Program.cs
@@ -1,4 +1,18 @@
 ﻿
 //Scaffold-DbContext "Data Source=D:\\VSprojects\\EFCoreConsoleApp\\EFCoreConsoleApp\\EFCoreConsoleApp\\DB\\helloapp.db" Microsoft.EntityFrameworkCore.Sqlite
 
+using EFCoreConsoleApp;
+
+using (HelloappContext db = new HelloappContext())
+{
+    var users = db.Users.ToList();
+
+    Console.WriteLine("Users:");
+
+    foreach (var u in users)
+    {
+        Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+    }
+}
+
 Console.ReadLine();
