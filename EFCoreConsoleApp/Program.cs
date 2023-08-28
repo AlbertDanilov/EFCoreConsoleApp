9504246760f172c8bd48febfc5deeecb29599bd0
Danilov <albert95danilov@gmail.com>
2023-08-28 17:48:12 +0300

    Добавьте файлы проекта.

---
+++ EFCoreConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using EFCoreConsoleApp.DAL;
+using EFCoreConsoleApp.Models;
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    User tom = new User { Name = "Tom", Age = 33 };
+    User alice = new User { Name = "Alice", Age = 26 };
+
+    db.Users.Add(tom);
+    db.Users.Add(alice);
+    db.SaveChanges();
+    Console.WriteLine("Objects saved");
+
+    var users = db.Users.ToList();
+    Console.WriteLine("Users:");
+    foreach (var user in users)
+    {
+        Console.WriteLine($"{user.Id}.{user.Name} - {user.Age}");
+    }
+
+    Console.ReadLine();
+}
