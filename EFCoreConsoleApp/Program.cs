4d718dee1e54acdd2dc0cde0da85c17c7b367f84
Danilov <albert95danilov@gmail.com>
2023-08-28 18:04:47 +0300

    Reverse Engineering

--- EFCoreConsoleApp/Program.cs
+++ EFCoreConsoleApp/Program.cs
@@ -1,30 +1,4 @@
-﻿using EFCoreConsoleApp.DAL;
-using EFCoreConsoleApp.Models;
+﻿
+//Scaffold-DbContext "Data Source=D:\\VSprojects\\EFCoreConsoleApp\\EFCoreConsoleApp\\EFCoreConsoleApp\\DB\\helloapp.db" Microsoft.EntityFrameworkCore.Sqlite
 
-using (ApplicationContext db = new ApplicationContext())
-{
-    /*  --Add & Remove
-    User tom = new User { Name = "Tom", Age = 33 };
-    User alice = new User { Name = "Alice", Age = 26 };
-
-    db.Users.Add(tom);
-    db.Users.Add(alice);
-    db.SaveChanges();
-    Console.WriteLine("Objects saved");
-
-    var users = db.Users.ToList();
-    Console.WriteLine("Users:");
-    foreach (var user in users)
-    {
-        Console.WriteLine($"{user.Id}.{user.Name} - {user.Age}");
-    }
-
-    db.Users.Remove(db.Users.ToList().Last());
-    db.SaveChanges();
-    */
-
-
-
-
-    Console.ReadLine();
-}
+Console.ReadLine();
