540b7b0bf27f9bfaf8b7f428f29664f7de72b7c7
Danilov <albert95danilov@gmail.com>
2023-08-29 17:13:35 +0300

    SqlServer

--- EFCoreConsoleApp/HelloappContext.cs
+++ EFCoreConsoleApp/HelloappContext.cs
@@ -8,47 +8,10 @@ namespace EFCoreConsoleApp;
 
 public partial class HelloappContext : DbContext
 {    
-    public HelloappContext()
-    {
-        Database.EnsureCreated();
-    }
-
-    public HelloappContext(DbContextOptions<HelloappContext> options)
-        : base(options)
-    {
-        //Database.EnsureDeleted();
-        //Database.EnsureCreated();
-    }
-
-    readonly StreamWriter logStream = new StreamWriter("mylog.txt", true);
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlite();
-        //optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
-        //optionsBuilder.LogTo(Console.WriteLine, new[] { RelationalEventId.CommandExecuted });
-        //optionsBuilder.LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name });
-        //optionsBuilder.LogTo(message => System.Diagnostics.Debug.WriteLine(message));
-        //optionsBuilder.LogTo(logStream.WriteLine);
+        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;");
     }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        OnModelCreatingPartial(modelBuilder);
-    }
-
-    public override void Dispose()
-    {
-        base.Dispose();
-        logStream.Dispose();
-    }
-
-    public override async ValueTask DisposeAsync()
-    {
-        await base.DisposeAsync();
-        await logStream.DisposeAsync();
-    }
-
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
--- EFCoreConsoleApp/Program.cs
+++ EFCoreConsoleApp/Program.cs
@@ -1,41 +1,28 @@
 ﻿
-//Scaffold-DbContext "Data Source=D:\\VSprojects\\EFCoreConsoleApp\\EFCoreConsoleApp\\EFCoreConsoleApp\\DB\\helloapp.db" Microsoft.EntityFrameworkCore.Sqlite
 
 using EFCoreConsoleApp;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
-var builder = new ConfigurationBuilder();
-builder.SetBasePath(Directory.GetCurrentDirectory());
-builder.AddJsonFile("appsettings.json");
-var config = builder.Build();
-
-string connectionString = config.GetConnectionString("DefaultConnection");
-
-var optionsBuilder = new DbContextOptionsBuilder<HelloappContext>();
-var options = optionsBuilder.UseSqlite(connectionString).Options;
-
-using (HelloappContext db = new HelloappContext(options))
+using (HelloappContext db = new HelloappContext())
 {
-    bool isDbAvalaible = db.Database.CanConnect();
+    db.Database.EnsureDeleted();
+    db.Database.EnsureCreated();
 
-    if (isDbAvalaible)
+    User tom = new User { Name = "Tom", Age = 21 };
+    User alice = new User { Name = "Alice", Age = 22 };
+
+    db.Users.AddRange(tom, alice);
+
+    db.SaveChanges();
+
+    var users = db.Users.ToList();
+    Console.WriteLine("Users:");
+
+    foreach (var user in users)
     {
-        //User tom = new User { Name = "Tom", Age = 21 };
-        //User alice = new User { Name = "Alice", Age = 22 };
-
-        //db.Users.Add(tom);
-        //db.Users.Add(alice);
-        //db.SaveChanges();
-
-        var users = db.Users.ToList();
-        Console.WriteLine("Users:");
-
-        foreach (var user in users)
-        {
-            Console.WriteLine($"{user.Id}.{user.Name} - {user.Age}");
-        }
-    }    
+        Console.WriteLine($"{user.Id}.{user.Name} - {user.Age}");
+    }
 }
 
 Console.ReadLine();
