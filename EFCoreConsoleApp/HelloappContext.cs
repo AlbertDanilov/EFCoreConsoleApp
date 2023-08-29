2d8a3c94590dda9ee19c7e2f9b24ca8c2bcfea3a
Albert Danilov <albert95danilov@gmail.com>
2023-08-29 07:40:54 +0300

    LogTo file.txt

--- EFCoreConsoleApp/HelloappContext.cs
+++ EFCoreConsoleApp/HelloappContext.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 namespace EFCoreConsoleApp;
 
 public partial class HelloappContext : DbContext
-{
+{    
     public HelloappContext()
     {
         Database.EnsureCreated();
@@ -18,18 +18,32 @@ public partial class HelloappContext : DbContext
         Database.EnsureCreated();
     }
 
+    readonly StreamWriter logStream = new StreamWriter("mylog.txt", true);
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite();
         //optionsBuilder.LogTo(Console.WriteLine);
-        optionsBuilder.LogTo(message => System.Diagnostics.Debug.WriteLine(message));
+        //optionsBuilder.LogTo(message => System.Diagnostics.Debug.WriteLine(message));
+        optionsBuilder.LogTo(logStream.WriteLine);
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         OnModelCreatingPartial(modelBuilder);
+    }
+
+    public override void Dispose()
+    {
+        base.Dispose();
+        logStream.Dispose();
+    }
+
+    public override async ValueTask DisposeAsync()
+    {
+        await base.DisposeAsync();
+        await logStream.DisposeAsync();
     }
 
     partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
