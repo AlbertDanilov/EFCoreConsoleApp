3f3f849ca29f89d2db93ae5a471926eb875d3923
Danilov <albert95danilov@gmail.com>
2023-08-30 15:10:06 +0300

    Fluent API, Аннотации

--- EFCoreConsoleApp/HelloappContext.cs
+++ EFCoreConsoleApp/HelloappContext.cs
@@ -14,4 +14,18 @@ public partial class HelloappContext : DbContext
     {
         optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;");
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //modelBuilder.Entity<User>().ToTable("Peoples");
+        //modelBuilder.Ignore<User>();
+        //modelBuilder.Entity<User>().HasKey(x => x.Id);
+        //modelBuilder.Entity<User>().HasKey(x => new {x.Id, x.Name});
+        //modelBuilder.Entity<User>().Ignore(x => x.Age);
+        //modelBuilder.Entity<User>().Property(x => x.Name).IsRequired();
+        //modelBuilder.Entity<User>().Property(x => x.Name).IsOptional();
+        //modelBuilder.Entity<User>().Property(x => x.Name).HasMaxLength(255);
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
